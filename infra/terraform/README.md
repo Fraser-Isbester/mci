d3b77fc0f9a09c669ce3ddd00bbd70417ba42d93
Fraser Isbester <fraserisbester@gmail.com>
2024-04-01 19:57:20 -0700

    Initial Commit 👾

---
+++ infra/terraform/README.md
@@ -0,0 +1,2 @@
+# Infra/Terraform Directory
+This Directory contains terraform that bootstraps the infrastructure. Most of the ifnrastructure is managed by by the Control Cluster.
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,6 @@
+
+# The Google Project ID. https://support.google.com/googleapi/answer/7014113?hl=en
+variable "project_id" {
+  description = "The Google project id."
+  type        = string
+}
